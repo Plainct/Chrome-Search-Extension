877f613c1436bfcfe1c73a1007ba15130c40672d
Plainct <120462767+Plainct@users.noreply.github.com>
2023-06-18 11:24:31 +0800

    增加网站forvo

--- popup.js
+++ popup.js
@@ -5,12 +5,13 @@ function search() {
     var url2 = 'https://www.weblio.jp/content/' + encodeURIComponent(query);
     var url3 = 'https://thesaurus.weblio.jp/content/' + encodeURIComponent(query);
     var url4 = 'https://cjjc.weblio.jp/content/' + encodeURIComponent(query);
+    var url5 = 'https://ja.forvo.com/search/' + encodeURIComponent(query);
 	
 
     window.open(url1, '_blank');
     window.open(url2, '_blank');
     window.open(url3, '_blank');
-    window.open(url4, '_blank');
+    window.open(url5, '_blank');
   }
 }
 
