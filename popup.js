f20add30b6c2a923c7dca4e1c37dade492dc1411
Plainct <120462767+Plainct@users.noreply.github.com>
2023-06-18 11:21:27 +0800

    upload main file

---
+++ popup.js
@@ -0,0 +1,30 @@
+function search() {
+  var query = document.getElementById('search-input').value;
+  if (query !== '') {
+    var url1 = 'https://news.yahoo.co.jp/search?p=' + encodeURIComponent(query) + '&ei=utf-8';
+    var url2 = 'https://www.weblio.jp/content/' + encodeURIComponent(query);
+    var url3 = 'https://thesaurus.weblio.jp/content/' + encodeURIComponent(query);
+    var url4 = 'https://cjjc.weblio.jp/content/' + encodeURIComponent(query);
+	
+
+    window.open(url1, '_blank');
+    window.open(url2, '_blank');
+    window.open(url3, '_blank');
+    window.open(url4, '_blank');
+  }
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('search-input').focus();
+});
+
+
+
+document.getElementById('search-input').addEventListener('keyup', function(event) {
+  if (event.keyCode === 13) {
+    search();
+  }
+});
+
+document.getElementById('search-btn').addEventListener('click', search);
