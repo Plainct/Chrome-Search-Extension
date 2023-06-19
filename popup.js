c9c689f4deae3f83240cf05c37bf4263d8dae4d6
Plainct <120462767+Plainct@users.noreply.github.com>
2023-06-19 16:41:28 +0800

    添加仅搜索Yahoo功能

--- popup.js
+++ popup.js
@@ -1,3 +1,4 @@
+
 function search() {
   var query = document.getElementById('search-input').value;
   if (query !== '') {
@@ -15,7 +16,23 @@ function search() {
     window.open(url5, '_blank');
   }
 }
-
+function searchyahoo() {
+	var query = document.getElementById('search-input').value;
+	if (query !== '') {
+		var urlyahoo = 'https://news.yahoo.co.jp/search?p=' + encodeURIComponent(query) + '&ei=utf-8';
+		// var urlwebliow = 'https://www.weblio.jp/content/' + encodeURIComponent(query);
+		// var urlwebliot = 'https://thesaurus.weblio.jp/content/' + encodeURIComponent(query);
+		// var urlweblioc = 'https://cjjc.weblio.jp/content/' + encodeURIComponent(query);
+		// var forvoja = 'https://ja.forvo.com/search/' + encodeURIComponent(query);
+	  
+  
+	  window.open(urlyahoo, '_blank');
+	//   window.open(url2, '_blank');
+	//   window.open(url3, '_blank');
+	//   window.open(url4, '_blank');
+	//   window.open(url5, '_blank');
+	}
+  }
 
 document.addEventListener('DOMContentLoaded', function() {
   document.getElementById('search-input').focus();
@@ -30,3 +47,4 @@ document.getElementById('search-input').addEventListener('keyup', function(event
 });
 
 document.getElementById('search-button').addEventListener('click', search);
+document.getElementById('search-yahoo').addEventListener('click', searchyahoo);
